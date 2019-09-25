16a9cf19603bfdbfd32e4cc1162d17261e395477
theJOHNwatt <j.watt10@gmail.com>
2019-09-24 18:36:32 -0700

    :art: Styled Header and nav

--- deforestation-dashboard/src/components/DashboardFooter.js
+++ deforestation-dashboard/src/components/DashboardFooter.js
@@ -2,26 +2,43 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 import Dashboard from "./Dashboard";
-const FooterDiv = styled.div``;
-const BackToTop = styled.div``;
+const FooterDiv = styled.div`
+// border:1px solid red;
+position: fixed;
+width: 100%;
+display: flex;
+justify-content: center;
+padding:0 10px;
+align-items: center;
+background-color: rgba(10,131,52,.8);
+
+a {
+  color:white;
+  margin:0 10px; 
+}
+`;
+
+const Links = styled.div `
+
+`;
 
 const DashboardFooter = () => {
   return (
-    <footer>
+    
       <FooterDiv>
-        <BackToTop>
+          {/* <Links>
+          <div>
           <NavLink to="#page-top"> Back To Top</NavLink>
-        </BackToTop>
-        <div>
+          </div>
           <NavLink to="/RegisterScreen" className="nav-link">
             Register
           </NavLink>
           <NavLink to="/register" className="nav-link">
             Login
           </NavLink>
-        </div>
+          </Links> */}
       </FooterDiv>
-    </footer>
+    
   );
 };
 
--- deforestation-dashboard/src/components/DashboardHeader.js
+++ deforestation-dashboard/src/components/DashboardHeader.js
@@ -1,35 +1,88 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
+import treeiconwhite from '../treeiconwhite.png';
 
-const NavBar = styled.div``;
+const NavBar = styled.div`
+// border: 1px solid red;
+display: flex;
+width: 100%;
+justify-content: space-between;
+padding:0 10px;
+align-items: center;
+background-color: #0a8334;
+height: 45px;
+color: #9d5f38;
+
+img {
+  width: 8%;
+  
+}
+img, b {
+  filter: drop-shadow(0 0 1px rgba(0,0,0,.5));
+}
+`;
+
+const Links = styled.div `
+// border: 1px solid red;
+width: 30%;
+display: flex;
+justify-content: space-between;
+a{
+  color:white;
+  font-size: 1.2rem;
+  font-weight:bold;
+  transition: color 0.2s linear;
+  -webkit-transition: color 0.2s linear;
+  -moz-transition: color 0.2s linear;
+  filter: drop-shadow(0 0 2px rgba(0,0,0,.5));
+}
+a:hover {
+  
+  color: #9d5f38;
+}
+img{
+  width:15px;
+  margin-right: 3px;
+}
+`;
+
+
 
 const HeaderText = styled.h1``;
 
 const DashboardHeader = () => {
   return (
     <NavBar className="navigation" id="page-top">
+      <div className='logo'> 
+      
       <HeaderText>
+       <img src={treeiconwhite} />
         <b>Deforestaion Dashboard</b>
       </HeaderText>
-      <nav>
+      </div>
+      <Links>
+     
         {/* ////////////// Need to add corect href for home and about pages ////////////  */}
         <NavLink className="nav-links" to="/">
+        <img src={treeiconwhite} />
           Home
         </NavLink>
 
         <NavLink className="nav-links" to="about the team page ">
+        <img src={treeiconwhite} />
           About
         </NavLink>
 
         <NavLink className="nav-links" to="">
+        <img src={treeiconwhite} />
           Maps By Country
         </NavLink>
-      </nav>
-      <div>
-        <NavLink to="/register">Register</NavLink>
-        <NavLink to="/">Login</NavLink>
-      </div>
+      
+      
+        <NavLink to="/register"><img src={treeiconwhite} />Register</NavLink>
+        <NavLink to="/"><img src={treeiconwhite} />Login</NavLink>
+      </Links>
     </NavBar>
   );
 };
