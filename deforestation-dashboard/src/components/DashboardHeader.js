023a9f56236e85fde5ce8151793c7d2628dbb15e
theJOHNwatt <j.watt10@gmail.com>
2019-09-26 13:08:51 -0700

    made header responsive

--- deforestation-dashboard/src/components/DashboardHeader.js
+++ deforestation-dashboard/src/components/DashboardHeader.js
@@ -14,14 +14,41 @@ background-color: #0a8334;
 height: 45px;
 color: #c96b00;
 box-shadow: 0 0 3px #333;
+@media screen and (max-width: 1000px) {
+  height: 35px;
+  padding: 0;
+  
+}
+@media screen and (max-width: 650px) {
+  flex-direction: column;
+  height: 65px;
+  padding-bottom: 5px;
+}
+
+
 
 
 img {
   width: 8%;
+  @media screen and (max-width: 1000px) {
+    width: 5%;
+  }
+  @media screen and (max-width: 650px){
+    width: 7%;
+  }
   
 }
 img, b {
   filter: drop-shadow(0 0 1px rgba(0,0,0,.5));
+}
+
+b {
+  @media screen and (max-width: 1000px){
+    font-size: 1.5rem;
+  }
+  @media screen and (max-width: 650px) {
+    font-size: 2rem;
+  }
 }
 `;
 
@@ -29,6 +56,12 @@ const Links = styled.div `
 width: 50%;
 display: flex;
 justify-content: space-around;
+@media screen and (max-width: 1000px) {
+  width: 60%;
+}
+@media screen and (max-width: 650px) {
+  width: 100%;
+}
 
 a{
   color:white;
@@ -38,6 +71,10 @@ a{
   -webkit-transition: color 0.2s linear;
   -moz-transition: color 0.2s linear;
   filter: drop-shadow(0 0 2px rgba(0,0,0,.5));
+  @media screen and (max-width: 1000px) {
+    font-size: 1rem;
+  }
+  
 }
 a:hover {
   
@@ -46,6 +83,12 @@ a:hover {
 img{
   width:15px;
   margin-right: 3px;
+  @media screen and (max-width: 1000px) {
+    display: none;
+  }
+  @media screen and (max-width: 650px) {
+    display: unset;
+  }
 }
 `;
 
