a4d6533b5ed95a3d9f1cf4757a8a5bef5bae5914
Tisha Holder <50963507+TishaHolder@users.noreply.github.com>
2019-09-27 12:27:01 -0400

    updates

--- deforestation-dashboard/src/components/Maps.js
+++ deforestation-dashboard/src/components/Maps.js
@@ -2,28 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-function Maps(props) {
+class Maps extends React.Component {
 
-  //console.log("maps props", year);
-  const { year } = props;
+  constructor(props){
+    super(props);
 
-  console.log("maps props", year);
-  
-  return(
 
-    <div class = "maps-div">     
 
-      <img className="prediction-map" src={require(`../maps/forest_coverage_${year}.png`)} width= "1000px" height = "auto"/>
+  }
 
-    </div>
-    
-  );  
+  render(){
+    return(
 
-  
+      <div class = "maps-div">
+
+        <img className="prediction-map" src={require(`../maps/forest_coverage_${this.props.year}.png`)} width= "1000px" height = "auto"/>
+
+      </div>
+
+
+    );
+
+  }
+
 }
 
-
 export default Maps;
+
 
 
 
